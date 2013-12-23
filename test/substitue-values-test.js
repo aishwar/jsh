1a7d2e8a23369e3fd31759da7d7eef16788b4f20
Aishwar Muthuraman <aishwar@floatingpoint.ca>
2013-12-23 01:17:23 -0500

    Add: substitute values in commands from property map

---
+++ test/substitue-values-test.js
@@ -0,0 +1,18 @@
+
+var substituteValues = require('../lib/substitute-values');
+var assert = require('assert');
+
+console.log('Tests stareted');
+
+assert.equal(substituteValues('hello', {}), 'hello', 
+  'No substitution');
+assert.equal(substituteValues('Howdy $name', {name: 'Aishwar'}), 'Howdy Aishwar', 
+  'Root level subsitution');
+assert.equal(substituteValues('Howdy $user.name', {user: {name: 'Aishwar'}}), 'Howdy Aishwar', 
+  'Nested object substitution');
+assert.equal(substituteValues('echo $(pwd)', {text: 'hello'}), 'echo $(pwd)',
+  'Unrecognized values left as is');
+assert.equal(substituteValues('hello'), 'hello', 
+  "Empty property map doesn't break");
+
+console.log('Tests completed :)');
